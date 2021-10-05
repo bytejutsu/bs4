6583b7b3598ba2aac6ab86486c3bcbef1b1e9f50
Dhia Nabli <dhianabli@outlook.com>
2021-10-05 08:20:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://news.ycombinator.com/")
+
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+print(largest_index)
+
+print(article_texts)
+print(article_links)
+print(article_upvotes)
+
+
+
